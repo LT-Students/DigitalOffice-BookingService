144e35118b28806fe74c52728169c3ee7d7df36c
Dmitry Solovyov <dmitry.solovyov@lanit-tercom.com>
2022-03-24 17:11:08 +0300

    add getting of database url and apiKey from environment variables

--- src/BookingService/Program.cs
+++ src/BookingService/Program.cs
@@ -1,20 +1,54 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using System;
 
-var builder = WebApplication.CreateBuilder(args);
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
 
-// Add services to the container.
+string seqServerUrl = Environment.GetEnvironmentVariable("seqServerUrl");
+if (string.IsNullOrEmpty(seqServerUrl))
+{
+    seqServerUrl = configuration["Serilog:WriteTo:1:Args:serverUrl"];
+}
 
-builder.Services.AddControllers();
+string seqApiKey = Environment.GetEnvironmentVariable("seqApiKey");
+if (string.IsNullOrEmpty(seqApiKey))
+{
+    seqApiKey = configuration["Serilog:WriteTo:1:Args:apiKey"];
+}
 
-var app = builder.Build();
+Log.Logger = new LoggerConfiguration().ReadFrom
+    .Configuration(configuration)
+    .Enrich.WithProperty("Service", "BookingService")
+    .WriteTo.Seq(
+        serverUrl: seqServerUrl,
+        apiKey: seqApiKey)
+    .CreateLogger();
 
-// Configure the HTTP request pipeline.
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
 
-app.UseHttpsRedirection();
+    builder.Services.AddControllers();
 
-app.UseAuthorization();
+    var app = builder.Build();
 
-app.MapControllers();
+    app.UseHttpsRedirection();
 
-app.Run();
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+}
+catch (Exception e)
+{
+    Log.Fatal(e, "Can not properly start BookingService.");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
