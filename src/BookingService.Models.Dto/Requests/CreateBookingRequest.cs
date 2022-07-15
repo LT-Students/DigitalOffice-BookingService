039c5886eaa2cac72c2441851948bfbe54d0d7bb
DmitrySolovyov <dmitry.solovyov@lanit-tercom.com>
2022-07-15 12:41:43 +0300

    add request

---
+++ src/BookingService.Models.Dto/Requests/CreateBookingRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LT.DigitalOffice.BookingService.Models.Dto.Requests
+{
+  public record CreateBookingRequest
+  {
+    public Guid WorkspaceId { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+  }
+}
