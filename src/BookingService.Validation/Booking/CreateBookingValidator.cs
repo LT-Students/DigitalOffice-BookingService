6fa44ae28b92c7cd982921c6d1b76676f1fbdaca
DmitrySolovyov <dmitry.solovyov@lanit-tercom.com>
2022-07-15 12:41:32 +0300

    add validator

---
+++ src/BookingService.Validation/Booking/CreateBookingValidator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using LT.DigitalOffice.BookingService.Models.Dto.Requests;
+using LT.DigitalOffice.BookingService.Validation.Booking.Interfaces;
+
+namespace LT.DigitalOffice.BookingService.Validation.Booking
+{
+  public class CreateBookingValidator : AbstractValidator<CreateBookingRequest>, ICreateBookingValidator
+  {
+  }
+}
---
+++ src/BookingService.Validation/Booking/Interfaces/ICreateBookingValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+using LT.DigitalOffice.BookingService.Models.Dto.Requests;
+using LT.DigitalOffice.Kernel.Attributes;
+
+namespace LT.DigitalOffice.BookingService.Validation.Booking.Interfaces
+{
+  [AutoInject]
+  public interface ICreateBookingValidator : IValidator<CreateBookingRequest>
+    {
+    }
+}
