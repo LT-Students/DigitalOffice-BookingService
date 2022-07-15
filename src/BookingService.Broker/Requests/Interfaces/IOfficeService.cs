e8a95d7585608da1c764ee4503d146718f260565
DmitrySolovyov <dmitry.solovyov@lanit-tercom.com>
2022-07-15 12:44:47 +0300

    add service for getting workspace info

---
+++ src/BookingService.Broker/Requests/Interfaces/IOfficeService.cs
@@ -0,0 +1,13 @@
+﻿using LT.DigitalOffice.Kernel.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LT.DigitalOffice.BookingService.Broker.Requests.Interfaces
+{
+  [AutoInject]
+    public interface IOfficeService
+    {
+      Task<Guid?> CheckWorkspaceExistence(Guid workspaceId, List<string> errors);
+    }
+}
---
+++ src/BookingService.Broker/Requests/OfficeService.cs
@@ -0,0 +1,34 @@
+﻿using LT.DigitalOffice.BookingService.Broker.Requests.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LT.DigitalOffice.Kernel.BrokerSupport.Helpers;
+using LT.DigitalOffice.Models.Broker.Requests.Office;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+
+namespace LT.DigitalOffice.BookingService.Broker.Requests
+{
+  public class OfficeService : IOfficeService
+  {
+    private readonly IRequestClient<ICheckWorkspaceExistenceRequest> _rcCheckWorkspaceExistence;
+    private readonly ILogger<OfficeService> _logger;
+
+    public OfficeService(
+      IRequestClient<ICheckWorkspaceExistenceRequest> rcCheckWorkspaceExistence,
+      ILogger<OfficeService> logger)
+    {
+      _rcCheckWorkspaceExistence = rcCheckWorkspaceExistence;
+      _logger = logger;
+    }
+
+    public async Task<Guid?> CheckWorkspaceExistence(Guid workspaceId, List<string> errors)
+    {
+      return (await RequestHandler.ProcessRequest<ICheckWorkspaceExistenceRequest, ICheckWorkspaceExistenceRequest>(
+        _rcCheckWorkspaceExistence,
+        ICheckWorkspaceExistenceRequest.CreateObj(workspaceId),
+        errors,
+        _logger))?.WorkspaceId;
+    }
+  }
+}
