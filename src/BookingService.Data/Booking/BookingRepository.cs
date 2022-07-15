87f943d266a94852251d130782a373200b33d3b7
DmitrySolovyov <dmitry.solovyov@lanit-tercom.com>
2022-07-15 12:44:25 +0300

    add repo

---
+++ src/BookingService.Data/Booking/BookingRepository.cs
@@ -0,0 +1,39 @@
+﻿using LT.DigitalOffice.BookingService.Data.Booking.Interfaces;
+using LT.DigitalOffice.BookingService.Data.Provider;
+using LT.DigitalOffice.BookingService.Models.Db;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace LT.DigitalOffice.BookingService.Data.Booking
+{
+  public class BookingRepository : IBookingRepository
+  {
+    private readonly IDataProvider _provider;
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    private readonly ILogger<BookingRepository> _logger;
+
+    public BookingRepository(
+      IDataProvider provider,
+      IHttpContextAccessor httpContextAccessor,
+      ILogger<BookingRepository> logger)
+    {
+      _provider = provider;
+      _httpContextAccessor = httpContextAccessor;
+      _logger = logger;
+    }
+
+    public async Task CreateAsync(DbBooking booking)
+    {
+      if (booking == null)
+      {
+        _logger.LogWarning(new ArgumentNullException(nameof(booking)).Message);
+        return;
+      }
+
+      _provider.Bookings.Add(booking);
+      await _provider.SaveAsync();
+    }
+  }
+}
---
+++ src/BookingService.Data/Booking/Interfaces/IBookingRepository.cs
@@ -0,0 +1,12 @@
+﻿using LT.DigitalOffice.BookingService.Models.Db;
+using LT.DigitalOffice.Kernel.Attributes;
+using System.Threading.Tasks;
+
+namespace LT.DigitalOffice.BookingService.Data.Booking.Interfaces
+{
+  [AutoInject]
+  public interface IBookingRepository
+    {
+      Task CreateAsync(DbBooking booking);
+    }
+}
