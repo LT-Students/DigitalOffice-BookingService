979d24eee58bdc4a6c5be17b6cf81b53f18ebe50
DmitrySolovyov <dmitry.solovyov@lanit-tercom.com>
2022-07-15 12:43:58 +0300

    add controller

---
+++ src/BookingService/Controllers/BookingController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using LT.DigitalOffice.BookingService.Business.Commands.Booking.Interfaces;
+using LT.DigitalOffice.BookingService.Models.Dto.Requests;
+using LT.DigitalOffice.Kernel.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LT.DigitalOffice.BookingService.Controllers
+{
+  [Route("[controller]")]
+  [ApiController]
+  public class BookingController : ControllerBase
+  {
+    [HttpPost]
+    public async Task<OperationResultResponse<Guid?>> Create(
+      [FromBody] CreateBookingRequest request,
+      [FromServices] ICreateBookingCommand command)
+    {
+      return await command.ExecuteAsync(request);
+    }
+  }
+}
