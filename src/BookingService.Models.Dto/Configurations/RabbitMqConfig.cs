2c3bcfb1caf5d000fbe34a728612b5a9571905ef
DmitrySolovyov <dmitry.solovyov@lanit-tercom.com>
2022-07-15 12:42:22 +0300

    add rabbitmq config class

---
+++ src/BookingService.Models.Dto/Configurations/RabbitMqConfig.cs
@@ -0,0 +1,9 @@
+﻿using LT.DigitalOffice.Kernel.BrokerSupport.Configurations;
+
+namespace LT.DigitalOffice.BookingService.Models.Dto.Configurations
+{
+  public class RabbitMqConfig : BaseRabbitMqConfig
+  {
+    public string AuthenticationServiceValidationUrl { get; set; }
+  }
+}
