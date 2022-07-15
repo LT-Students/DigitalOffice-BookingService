9760955f76839bcbcb8b36ec2648947863304051
DmitrySolovyov <dmitry.solovyov@lanit-tercom.com>
2022-07-15 12:43:04 +0300

    add db model for booking

---
+++ src/BookingService.Models.Db/DbBooking.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using LT.DigitalOffice.Models.Broker.Models.Office;
+
+namespace LT.DigitalOffice.BookingService.Models.Db
+{
+  public class DbBooking
+  {
+    public const string TableName = "Bookings";
+    public Guid Id { get; set; }
+    public Guid WorkspaceId { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public Guid CreatedBy { get; set; }
+    public DateTime CreatedAtUtc { get; set; }
+    public bool IsActive { get; set; }
+  }
+
+  public class DbBookingConfiguration : IEntityTypeConfiguration<DbBooking>
+  {
+    public void Configure(EntityTypeBuilder<DbBooking> builder)
+    {
+      builder.ToTable(DbBooking.TableName);
+
+      builder.HasKey(t => t.Id);
+
+      builder.Property(w => w.WorkspaceId).IsRequired();
+
+      builder.Property(st => st.StartTime).IsRequired();
+
+      builder.Property(et => et.EndTime).IsRequired();
+    }
+  }
+}
