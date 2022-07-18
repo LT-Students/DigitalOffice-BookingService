67af3a28af20e2df50688c35711d86d8f8715927
Dmitry Solovyov <dmitry.solovyov@lanit.tercom.com>
2022-07-18 20:40:53 +0300

    add consumer endpoint

--- src/BookingService.Models.Dto/Configurations/RabbitMqConfig.cs
+++ src/BookingService.Models.Dto/Configurations/RabbitMqConfig.cs
@@ -1,9 +1,12 @@
-﻿using LT.DigitalOffice.Kernel.BrokerSupport.Configurations;
+﻿using LT.DigitalOffice.Kernel.BrokerSupport.Attributes;
+using LT.DigitalOffice.Kernel.BrokerSupport.Configurations;
+using LT.DigitalOffice.Models.Broker.Common;
 
 namespace LT.DigitalOffice.BookingService.Models.Dto.Configurations
 {
   public class RabbitMqConfig : BaseRabbitMqConfig
   {
-    public string AuthenticationServiceValidationUrl { get; set; }
+    [AutoInjectRequest(typeof(ICheckWorkspaceIsBookable))]
+    public string CheckWorkspaceIsBookableEndpoint { get; set; }
   }
 }
